facb5a4553e1d24d6ccb934d0f95ca1f870e46c5
GaelleLeroux <gaelleleroux974@gmail.com>
2022-10-21 11:23:45 +0200

    debut d'un truc

---
+++ TP5/src/repertoire.c
@@ -0,0 +1,35 @@
+# include <sys/types.h>
+# include <dirent.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "repertoire.h"
+
+void lire_dossier(const char * repertoire){
+    printf("on veut lire : %s \n",repertoire);
+    DIR *dirp ;
+    struct dirent * ent;
+
+    dirp = opendir(repertoire);
+
+    if (dirp == NULL){
+        perror("opendir");
+        //return(EXIT_FAILURE);
+    }
+
+    
+    while(1){
+        ent = readdir(dirp);
+        if (ent == NULL){
+            break;
+        }
+        printf("%s\n",ent->d_name);
+    }
+    closedir(dirp);
+
+}
+
+int main(){
+    lire_dossier("cd ..");
+    return(0);
+}
